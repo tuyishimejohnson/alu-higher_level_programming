fefb743d330299e5c42c18fd0471690c7f7e8235
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-06 06:08:03 -0800

    Initial

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,11 +1,3 @@
 #!/usr/bin/python3
 
-for letter in range(97, 123):
-
-        if letter == 101 or letter == 113:
-
-                    continue
-
-                    else:
-
-                                print("{}".format(chr(letter)), end="")
+print(*[chr(i) for i in range(97, 123) if chr(i) not in 'qe'], sep='')
