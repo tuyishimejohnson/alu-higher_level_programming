ccd63b8acba38ba0efd8874ab3f4ffd84094d5f7
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-06 02:36:30 -0800

    Hard to Know

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-number = 2
+
 if number > 0: 
-   print("is positive")
+   
+    print("{} is positive".format(number))
+
 elif number == 0:
-   print("is zero")
-else number < 0:
-   print("is negative")
+   
+    print("{} is zero".format(number))
+
+else: 
+   
+    print("{} is negative".format(number))
 
