4f3a5b23512f2fc90391045e6e1b0be6a65d3654
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-06 05:33:39 -0800

    Read me

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 for letter in range(97, 123):
 
-        if letter == 101 or letter == 113:
+if letter == 101 or letter == 113:
 
-                    continue
+         continue
 
-                    else:
+     else:
 
-                                print("{}".format(chr(letter)), end="")
+         print("{}".format(chr(letter)), end="")
