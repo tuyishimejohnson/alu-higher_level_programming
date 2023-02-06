23e9635c887fd9abea6f5eabb95d6263e3ea97f8
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-06 03:00:14 -0800

    Antropology

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+"last digit of" number "is" "number"
+
+if "last digit" > 5: 
+    
+    print("and is greater than 5")
+
+elif "last digit" == 0 
+    
+    print("and is 0")
+
+else:
+
+    print("and is less than 6 and not 0)
+
