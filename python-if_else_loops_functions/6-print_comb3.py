96eeec24f584cb9e6801863cd08734d0facd19bc
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-06 06:30:54 -0800

    Refer

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 
-for i in range(10):
+for i in range(9):
 
         for j in range(i + 1, 10):
 
-                    print("{:02d}".format(i * 10 + j), end=", ")
+                    if i < 8:
 
-                    print("89")
+                                    print("{:d}{:d}".format(i, j), end=", ")
+
+                                            else:
+
+                                                  print("{:d}{:d}".format(i, j))
