957be99731a86d09e460ebe08bc6c8dbea8cc89b
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-07 00:25:47 -0800

    Function that prints 1 to 100

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -4,18 +4,18 @@ def fizzbuzz():
 
     for i in range(1, 101):
 
-        if i % 3 == 0 and i % 5 == 0:
+        if i % 15 == 0:
 
-            print(Fizzbuzz, end' ')
+            print("FizzBuzz", end=" ")
 
         elif i % 3 == 0:
 
-            print("Fizz", end=' ')
+            print("Fizz", end=" ")
 
         elif i % 5 == 0:
 
-            print("Buzz", end=' ')
+            print("Buzz", end=" ")
 
         else:
 
-            print(i, end=' ')
+            print("{:d}".format(i), end=" ")
