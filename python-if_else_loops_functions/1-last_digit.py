3336628c7e6825b60f3391c9d4a66e24c805ddac
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-07 00:41:57 -0800

    Variable number

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -4,17 +4,32 @@ import random
 
 number = random.randint(-10000, 10000)
 
-last_digit = abs(number) % 10
 
-if last_digit > 5: 
 
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+if number > 0:
 
-elif last_digit == 0: 
-
-    print("Last digit of {} is {} and is 0".format(number, last_digit))
+    digit = number % 10
 
 else:
 
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    digit = ((number * -1) % 10) * -1
+
+
+
+print("Last digit of {:d} is {:d}".format(number, digit), end=" ")
+
+
+
+if digit == 0:
+
+    print("and is 0")
+
+elif number < 0 or digit < 6:
+
+    print("and is less than 6 and not 0")
+
+else:
+
+    print("and is greater than 5")
+
 
