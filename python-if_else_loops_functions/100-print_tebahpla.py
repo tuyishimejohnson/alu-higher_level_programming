bf506102c7b9eacaf0d6681286630f062bd616f5
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-09 04:53:15 -0800

    ascii alphabet in reverse order

--- python-if_else_loops_functions/100-print_tebahpla.py
+++ python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-
-
-def main():
-
-    for i in range(122, 96, -1):
-
-        if i % 2 == 0:
-
-    print("{}".format(chr(i)), end="")
-
-        else:
-
-            print("{}".format(chr(i-32)), end="")
-
-
-if __name__ == '__main__':
-
-    main()
+for letter in range(122, 96, -1):
+    if letter % 2 != 0:
+        letter -= 32
+print("{}".format(chr(letter)), end="")
