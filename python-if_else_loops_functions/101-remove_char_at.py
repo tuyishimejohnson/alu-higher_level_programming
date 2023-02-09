434083bbabecc6cd68d32862d84b57f7158bed94
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-09 05:24:16 -0800

    Function for creating a copy of file

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-
 def remove_char_at(str, n):
+    i = 0
+    new = ""
+    for char in string:
+        if i != n:
+            new += char
+        i += 1
+    return (new)
 
-    if n < 0 or n >= len(str):
-
-        return str
-
-    return str[:n] + str[n+1:]
