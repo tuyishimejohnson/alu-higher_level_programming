63b81f3c6255e7d577f4b77e108e9f255569982e
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-09 03:59:05 -0800

    Uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
+
 def uppercase(str):
+
     for i in range(len(str)):
+
         if ord(str[i]) >= 97 and ord(str[i]) <= 122:
+
             str = str[:i] + chr(ord(str[i]) - 32) + str[i+1:]
+
     print("{}\n".format(str), end="")
-    
+
 
