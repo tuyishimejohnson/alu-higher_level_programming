22859cf9e12c8fbdf906be285f678143dd5c3f05
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-20 05:32:54 -0800

    First toolbox

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -5,15 +5,7 @@ from calculator_1 import add, sub, mul, div
 
     a = 10
     b = 5
-
-def add(a, b):
-    print('{} + {} = {}'.format add(a, b))
-
-def sub(a, b):
-    print('{} - {} = {}'.format sub(a,b))
-
-def mul(a, b):
-    print("{} * {} = {}".format mul(a, b))
-
-def div(a, b):
-    print('{} // {} = {}'.format div(a, b))
+    print('{} + {} = {}'.format(a, b add(a, b)))
+    print('{} - {} = {}'.format(a, b sub(a,b)))
+    print("{} * {} = {}".format(a, b mul(a, b)))
+    print('{} // {} = {}'.format(a, b div(a, b)))
