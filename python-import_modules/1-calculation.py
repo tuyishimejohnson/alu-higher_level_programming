98a0a770e1fb567b7620e4dc03ee4c97c931cbcd
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-20 05:25:59 -0800

    First toolbox

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+if __name__ == '__main__'
+from calculator_1 import add, sub, mul, div
+
+    a = 10
+    b = 5
+
+def add(a, b):
+    print('{} + {} = {}'.format add(a, b))
+
+def sub(a, b):
+    print('{} - {} = {}'.format sub(a,b))
+
+def mul(a, b):
+    print("{} * {} = {}".format mul(a, b))
+
+def div(a, b):
+    print('{} // {} = {}'.format div(a, b))
