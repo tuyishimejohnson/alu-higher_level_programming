48e5fa7bc59f8fc77468cdd365f2ebf522194fa1
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-23 02:19:51 -0800

    Can you see me?

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def no_c(mys_string):
+    my_string = ('A', 'Z')
+    for i in my_string:
+        print(s.translate({ord('C'): None}))
+    while my_string = ('a', 'z')
+        print(my_string.translate({ord('c'): None}))
+   
