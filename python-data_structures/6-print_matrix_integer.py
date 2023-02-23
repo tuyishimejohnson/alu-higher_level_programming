10ff27dabdbb02e883baa6f693014a8fc2ad9c5c
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-23 01:52:53 -0800

    Lists of lists

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    ["numpy. array(matrix).format(print_natrix_integer)", '{:d}']
+    for row in matrix:
+        for i in range(len(row)):
+            print("{:d}".format(row[i]), end=" " if i != len(row)-1 else "")
+        
+        print()
+
