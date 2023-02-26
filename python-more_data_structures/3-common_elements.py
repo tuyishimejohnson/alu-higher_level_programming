8cb59ea5539b84e17658c050edb496577bb6b150
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-26 01:53:54 -0800

    Returns a set of common elements in 2

--- python-more_data_structures/3-common_elements.py
+++ python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    set_1 = {}
-    set_2 = {}
-    c_set = common_elements(set_1, set_2)
-    print(c_set)
+    common_set = set()
+    for elem in set_1:
+        if elem in set_2:
+            common_set.add(elem)
+    return common_set
+
