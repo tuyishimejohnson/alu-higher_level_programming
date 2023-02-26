cec4779780a52e716b1210a862240fa494416585
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-26 02:04:54 -0800

    Only different

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -2,7 +2,7 @@
 def only_diff_elements(set_1, set_2):
     unique_set = set()
     for elem in set_1:
-         if elem not in set_2:
+        if elem not in set_2:
             unique_set.add(elem)
     for elem in set_2:
         if elem not in set_1:
