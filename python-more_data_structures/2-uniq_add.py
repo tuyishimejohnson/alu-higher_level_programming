cf1df7e7984da878f062ecd7a20d7cdc97c9b88d
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-26 00:26:01 -0800

    Unique addition

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
     unique_integers = set()   
+
     for element in my_list:
         if element not in unique_integers:
             unique_integers.add(element)    
+
     sum_of_unique_integers = sum(unique_integers)
     return sum_of_unique_integers
