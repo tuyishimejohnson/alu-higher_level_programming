f723b4a61b2ccde289df59aad7d4987226f09158
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-25 23:43:51 -0800

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    result_matrix = [[0 for j in range(len(matrix[0]))] for i in range(len(matrix))]
-    for i in range(len(matrix)):
-        for j in range(len(matrix[0])):
-            result_matrix[i][j] = matrix[i][j] ** 2
-    
-    return result_matrix
+    num_rows = len(matrix)
+    num_cols = len(matrix[0])
+    squared_matrix = [[0] * num_cols for _ in range(num_rows)]
+    for i in range(num_rows):
+        for j in range(num_cols):
+            squared_matrix[i][j] = matrix[i][j] ** 2
+            
+    return squared_matrix
+
