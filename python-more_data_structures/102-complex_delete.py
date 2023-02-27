0427ab34c23984c7c06fccd7d3567b90cfeb5b34
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-27 03:37:36 -0800

    Delete a function with a specific value

--- python-more_data_structures/102-complex_delete.py
+++ python-more_data_structures/102-complex_delete.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in list(a_dictionary.keys()):
-        if a_dictionary[key] == value:
-            del a_dictionary[key]
+    keys_to_delete = []
+    for key, val in a_dictionary.items():
+        if val == value:
+            keys_to_delete.append(key)
+    for key in keys_to_delete:
+        del a_dictionary[key]
+    return a_dictionary
