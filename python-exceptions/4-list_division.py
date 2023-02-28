e087b06ecf9daf9adb5546c140f1f47b8dee4d66
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-28 00:21:03 -0800

    Function that divides element by element

---
+++ python-exceptions/4-list_division.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for i in range(list_length):
+        try:
+            num1 = my_list_1[i] if i < len(my_list_1) else 0
+            num2 = my_list_2[i] if i < len(my_list_2) else 0
+            result = num1 / mum2
+        except TypeError:
+            print('Wrong type')
+            result_list.append(0)
+        except ZeroDivisionError:
+            print('division by 0')
+            result_list.append(0)
+        except IndexError:
+            print('out of range')
+            result_list.append(0)
+        finally:
+            pass
+    return result_list
