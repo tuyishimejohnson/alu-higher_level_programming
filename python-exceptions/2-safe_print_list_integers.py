3d5995db1873ef31c1c559661aadd78f86f3eada
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-02-27 23:54:17 -0800

    Print and count integers

---
+++ python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x = 0):
+    count=0
+    for i in range(x):
+        try:
+            if type(my_list[i]) == int:
+                print('{:d}'.format(my_list[i]), end=" ")
+                count += 1
+        except IndexError:
+            raise ValueError('x exceeds the length of my_list')
+    print()
+    return count
