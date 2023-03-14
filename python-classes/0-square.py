2860c5252798b5ff6209ac51201df8c2b847d8f8
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-14 03:36:54 -0700

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
-"""
-This class defines a square.
-Attributes:
-    side (int or float): The length of each side of the square.
-    
-"""
+"""A class to create an object"""
 
 class Square:
+    """Detailed description"""
     pass
 
     
