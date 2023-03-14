59084f2a1e21121bd8438890ff56185cc21d3046
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-14 02:17:24 -0700

    square with size

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
+        'THe __init__ method initializes the size value
+        self.__size = size
+        Attributes: size'
         self.__size = size
 
