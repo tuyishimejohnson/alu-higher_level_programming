cd584a9d0bdee929a1eea1263f4ff007b71b72ef
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-14 03:15:57 -0700

    My first square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,40 @@
 #!/usr/bin/python3
-"""Square Class
-
-This class don't do anything for the moment
-
-"""
-
-Attributes:
-    attr: Square
-
-
 class Square:
-    pass
+    """
+    This class defines a square.
+
+    Attributes:
+        side (int or float): The length of each side of the square.
+
+    Methods:
+        perimeter(): Returns the perimeter of the square.
+        area(): Returns the area of the square.
+    """
+
+    def __init__(self, side):
+        """
+        Initializes a new instance of the Square class.
+
+        Args:
+            side (int or float): The length of each side of the square.
+        """
+        self.side = side
+
+    def perimeter(self):
+        """
+        Calculates the perimeter of the square.
+
+        Returns:
+            The perimeter of the square.
+        """
+        return 4 * self.side
+
+    def area(self):
+        """
+        Calculates the area of the square.
+
+        Returns:
+            The area of the square.
+        """
+        return self.side ** 2
+
