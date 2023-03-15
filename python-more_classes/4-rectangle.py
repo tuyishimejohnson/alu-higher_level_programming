adbab26a05c830c705b02443f7a5c63cb34e7956
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-15 02:41:17 -0700

    Eval is magic

--- python-more_classes/4-rectangle.py
+++ python-more_classes/4-rectangle.py
@@ -3,13 +3,15 @@
 
 
 class Rectangle:
-    """Initializing the attributes"""
+    """Initializing attributes"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+        
     @property
     def width(self):
         return self._width
+    
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -18,9 +20,11 @@ class Rectangle:
             raise ValueError("width must be >= 0")
         else:
             self._width = value
+        
     @property
     def height(self):
         return self._height
+    
     @height.setter
     def height(self, value):
         if not isinstance(value, int):
@@ -29,17 +33,21 @@ class Rectangle:
             raise ValueError("height must be >= 0")
         else:
             self._height = value
+    
     def area(self):
         return self.width * self.height
+    
     def perimeter(self):
         if self.width == 0 or self.height == 0:
             return 0
         else:
             return 2 * (self.width + self.height)
+        
     def __str__(self):
         if self.width == 0 or self.height == 0:
             return ""
         else:
             return "\n".join(["#" * self.width] * self.height)
+        
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
