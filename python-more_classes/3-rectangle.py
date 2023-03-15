2714fb28a3a9e461f4b643b31abd91f29070ce4d
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-15 01:41:05 -0700

    Area and Perimeter

---
+++ python-more_classes/3-rectangle.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""Starting a class builtin"""
+class Rectangle:
+    """Initializing attributes of the method"""
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self._width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value < 0:
+            raise ValueError('width must be >= 0')
+        self._width = value
+
+    @property
+    def height(self):
+        return self._height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value < 0:
+            raise ValueError('height must be >= 0')
+        self._height = value
+
+    def area(self):
+        return self.width * self.height
+
+    def perimeter(self):
+        if self.width == 0 or self.height == 0:
+            return 0
+        return 2 * (self.width + self.height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ''
+        rectangle = ''
+        for i in range(self.height):
+            rectangle += '#' * self.width + '\n'
+        return rectangle.rstrip()
+
+    def __repr__(self):
+        return f'Rectangle({self.width}, {self.height})'
