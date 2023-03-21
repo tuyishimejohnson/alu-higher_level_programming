86d974d4c42062f0f189808130822561058ba206
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-21 04:16:50 -0700

    Return the list

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,10 +1,4 @@
 #!/usr/bin/python3
 def lookup(obj):
     """Returns a list of available attributes and methods of an object"""
-    attributes = []
-    for attr in dir(obj):
-        # exclude private and built-in attributes
-        if not attr.startswith('_'):
-            attributes.append(attr)
-    return attributes
-
+    return dir(obj)
