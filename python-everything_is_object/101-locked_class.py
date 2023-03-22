0538ab8823397617654627f01b5c75753f6ed3bb
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-22 06:39:13 -0700

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Defining a class"""
+
+
 class LockedClass(object):
-    """using a builtin function"""
+    """using a builtin function to prevent creating new instance attributes"""
     __slots__ = 'first_name'
