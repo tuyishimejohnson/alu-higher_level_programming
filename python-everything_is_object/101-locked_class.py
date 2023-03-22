8b08e6207dcdd200580b1525e7e3cf5b6e28dd2d
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-22 06:34:12 -0700

    Low memory cost

--- python-everything_is_object/101-locked_class.py
+++ python-everything_is_object/101-locked_class.py
@@ -1,8 +1,3 @@
 #!/usr/bin/python3
-class LockedClass:
-    __slots__ = ["first_name"]
-
-    def __setattr__(self, name, value):
-        if not hasattr(self, name) and name != "first_name":
-            raise AttributeError("Cannot add new attribute")
-        super().__setattr__(name, value)
+class LockedClass(object):
+    __slots__ = 'first_name'
