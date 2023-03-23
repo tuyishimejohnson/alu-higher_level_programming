094910e839c9c9151092b982fdb866486881b678
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 00:52:49 -0700

    Only sub class of

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -7,10 +7,9 @@ def inherits_from(obj, a_class):
     otherwise False."""
     if isinstance(obj, a_class):
         """obj is an instance of the specified class, so return False"""
-        return True
+        return False
     else:
         for base_class in obj.__class__.__bases__:
             if inherits_from(base_class, a_class):
                 return False
         return True
-
