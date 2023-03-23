8228bf0387ff691a2554a8ee6b854bdbfd51968a
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-22 23:56:58 -0700

    Class mylist

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+"""Starting a class"""
 class MyList(list):
+    """Defining a class function for sorting"""
     def print_sorted(self):
         sorted_list = sorted(self)
         print(sorted_list)
