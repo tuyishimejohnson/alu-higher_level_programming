9dc5057aa355c51b456aaf0bbc61551821ecd9e5
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 07:43:31 -0700

    Load, add and save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
-"""Add all arguments to a Python list and save them to a file."""
-import sys
+from json import dumps
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-        __import__('6-load_from_json_file').load_from_json_file
 
-    try:
-        tems = load_from_json_file("add_item.json")
-    except FileNotFoundError:
-        tems = []
-    tems.extend(sys.argv[1:])
-    save_to_json_file(tems, "add_item.json")
+def save_to_json_file(my_obj, filename):
+    with open(filename, mode='w', encoding='utf-8') as f:
+        f.write(dumps(my_obj))
