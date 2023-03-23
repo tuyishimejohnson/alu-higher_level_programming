1c2177072098cc406967650d9aab399e7caf45d3
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 11:42:05 -0700

    Pascal's triangle

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -1,9 +1,14 @@
 #!/usr/bin/python3
 """Creating a function of integer list"""
-
-
 def pascal_triangle(n):
-    """Returning an empty list"""
-    for i in range(n):
-        if n <= 0:
-            return []
+    if n <= 0:
+        return []
+    else:
+        triangle = [[1]]
+        for i in range(1, n):
+            row = [1]
+            for j in range(1, i):
+                row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(1)
+            triangle.append(row)
+        return triangle
