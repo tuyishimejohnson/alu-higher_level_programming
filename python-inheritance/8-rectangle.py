8d40c4ba515a8f518fe95f894c02e09a83611fc4
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 02:15:38 -0700

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -5,14 +5,6 @@
 class Rectangle(BaseGeometry):
     """Initializing a rectangle with height and width"""
     def __init__(self, width, height):
-        """Args:
-        width (int): The width of the rectangle.
-        height (int): The height of the rectangle.
-
-        Raises:
-        TypeError: If the width or height is not an integer.
-        ValueError: If the width or height is less than or equal to 0.
-        """
         self.__width = self.integer_validator("width", width)
         self.__height = self.integer_validator("height", height)
 
