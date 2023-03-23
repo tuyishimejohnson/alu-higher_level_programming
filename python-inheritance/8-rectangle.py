dd1cd6288a03ae5a023ccd89120351bf15e27322
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 02:18:08 -0700

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,18 +1,11 @@
 #!/usr/bin/python3
-"""Class representing a rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Initializing a rectangle with height and width"""
     def __init__(self, width, height):
-        self.__width = self.integer_validator("width", width)
-        self.__height = self.integer_validator("height", height)
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
 
-    def area(self):
-        """
-        Calculates the area of the rectangle.
-
-        Returns:
-        The area of the rectangle.
-        """
-        return self.__width * self.__height
+        self.__width = width
+        self.__height = height
