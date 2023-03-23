abb57354701cd518251b6764039146236eb62352
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 12:14:59 -0700

    Load, add and save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""
-Script that adds all arguments to a Python list and saves them to a file.
-"""
-import json
+"""Add all arguments to a Python list and save them to a file."""
 import sys
-from os import path
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
-my_list = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-if path.exists(filename):
-    my_list = load_from_json_file(filename)
-
-for arg in sys.argv[1:]:
-    my_list.append(arg)
-
-save_to_json_file(my_list, filename)
-
+    try:
+        tems = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        tems = []
+    tems.extend(sys.argv[1:])
+    save_to_json_file(tems, "add_item.json")
