b9ae3e64d4738d32f7e9f70c71ea5ce1a7c8ac8b
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 00:43:20 -0700

    Only sub class of

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Initializing a function"""
+def inherits_from(obj, a_class):
+    """
+    Returns True if obj is an instance of a class that inherited (directly or indirectly) from a_class;
+    otherwise False.
+    """
+    if isinstance(obj, a_class):
+        """obj is an instance of the specified class, so return False"""
+        return False
+    else:
+        for base_class in obj.__class__.__bases__:
+            if inherits_from(base_class, a_class):
+                return True
+        return False
+
