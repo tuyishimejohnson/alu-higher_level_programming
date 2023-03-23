e13042b6799d544f658d25da529fa0d265f93509
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-22 23:37:42 -0700

    Lookup

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def lookup(obj):
-    """
-    Returns the list of available attributes and methods of an object
-    """
+"""Defining a function"""
 
+
+def lookup(obj):
+    """Returns the list of available attributes and methods of an object"""
     return dir(obj)
