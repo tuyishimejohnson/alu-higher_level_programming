3b3d7a0f1ca547bb687fdb219ca3019166b276c1
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 06:28:22 -0700

    To json string

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Importing json function"""
+
+
+import json
+
+def to_json_string(my_obj):
+    """
+    Converts an object to its JSON string representation.
+
+    Args:
+        my_obj: Any object that can be serialized to JSON.
+
+    Returns:
+        A JSON string representation of the object.
+    """
+    return json.dumps(my_obj)
