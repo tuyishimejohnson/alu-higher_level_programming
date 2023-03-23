b3379a37e24db2aec9dd8634c91df37d3bbd0576
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 02:50:00 -0700

    Rectangle

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,34 +1,11 @@
 #!/usr/bin/python3
-"""Defining a BaseGeometry class"""
-
-
-class BaseGeometry:
-    """Initializing items in area"""
-    def area(self):
-        raise NotImplementedError
-
-    def perimeter(self):
-        raise NotImplementedError
-
-    def integer_validator(self, name, value):
-        if not isinstance(value, int):
-            raise TypeError(f"{name} height must be an integer")
-        if value <= 0:
-            raise ValueError(f"{name} width must be greater than 0")
-
-"""Defining a Rectangle class"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
-    """Initializing the values in Rectangle"""
     def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
         self.integer_validator("width", width)
         self.integer_validator("height", height)
 
-    def area(self):
-        return self.__width * self.__height
-
-    def perimeter(self):
-        return 2 * (self.__width + self.__heiight)
+        self.__width = width
+        self.__height = height
