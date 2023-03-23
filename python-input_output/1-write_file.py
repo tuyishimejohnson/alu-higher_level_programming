fe5f1eaa17fa23f10979fc73ef3c4fdf21fce858
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 06:11:43 -0700

    Write to a file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+function that writes a string to a text file
+"""
+
+
+def write_file(filename="", text=""):
+    """ module write_file """
+    with open(filename, 'w') as b:
+        return b.write(text)
