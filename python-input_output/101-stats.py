c97764c8235f41dcdf816027ccf82b8c4cb4387f
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 12:32:31 -0700

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
-"""Importing sys and signal"""
+''' prasing the lofg '''
+
 
 import sys
-import signal
 
-status_codes = [200, 301, 400, 401, 403, 404, 405, 500]
-status_counts = {c: 0 for c in status_codes}
-total_size = 0
-line_count = 0
-"""Defining the status of the function"""
-def print_stats():
-    print(f"File size: {total_size}")
-    for code in sorted(status_codes):
-        count = status_counts[code]
-        if count > 0:
-            print(f"{code}: {count}")
 
-def handle_sigint(signum, frame):
-    print_stats()
-    sys.exit(0)
+def print_status():
+    '''
+        Printing the status of the request
+    '''
+    counter = 0
+    size = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
 
-signal.signal(signal.SIGINT, handle_sigint)
+    for l in sys.stdin:
+        line = l.split()
+        try:
+            size += int(line[-1])
+            code = line[-2]
+            status_codes[code] += 1
+        except:
+            continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
-for line in sys.stdin:
-    try:
-        _, _, _, status, size = line.strip().split()
-        status = int(status)
-        size = int(size)
-    except ValueError:
-        continue
-    if status in status_counts:
-        status_counts[status] += 1
-    total_size += size
-    line_count += 1
-    if line_count % 10 == 0:
-        print_stats()
+
+if __name__ == "__main__":
+    print_status()
