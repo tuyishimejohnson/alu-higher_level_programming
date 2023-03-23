fbed0381edf7382a15950632d69606849a48cdb3
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 06:38:14 -0700

    To json string

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -4,7 +4,8 @@
 
 def append_write(filename="", text=""):
     """Returning the number of characters"""
-    with open(filename, mode='a', encoding='utf-8') as file:
 
+
+    with open(filename, mode='a', encoding='utf-8') as file:
         file.write(text)
         return len(text)
