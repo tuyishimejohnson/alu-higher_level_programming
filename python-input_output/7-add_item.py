0ea399a516beed763c433911afe43241e5e97ff1
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 07:45:56 -0700

    Load, add and save

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Importing a function from a json file"""
+
+
 from json import dumps
 
-
+"""Defining a function to add arguments"""
 def save_to_json_file(my_obj, filename):
     with open(filename, mode='w', encoding='utf-8') as f:
         f.write(dumps(my_obj))
