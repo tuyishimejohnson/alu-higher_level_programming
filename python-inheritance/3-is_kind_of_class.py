bf7a6593b51521ed8e0681c1b5a60d7d3970f711
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 00:31:42 -0700

    Same class or inherit from

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -4,5 +4,5 @@
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True if the object is an instance of, or if object that a class that inherited from, the specified class; otherwise False."""
+    Returns True if the object is an instance of, the specified class; otherwise False."""
     return isinstance(obj, a_class) or issubclass(type(obj), a_class)
