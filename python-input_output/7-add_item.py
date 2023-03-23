ba0678ec0ea55ccc0d6b13be47be848e98dfdb58
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 07:19:34 -0700

    Load and add and save

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import json
+import sys
+from os import path
+from typing import List
+
+def save_to_json_file(my_obj: any, filename: str):
+    with open(filename, mode='w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
+
+def load_from_json_file(filename: str):
+    with open(filename, mode='r', encoding='utf-8') as file:
+        return json.load(file)
+
+def add_item(argv: List[str]):
+    my_list = []
+    if path.isfile('add_item.json'):
+        my_list = load_from_json_file('add_item.json')
+    my_list.extend(argv[1:])
+    save_to_json_file(my_list, 'add_item.json')
+    return my_list
+
+if __name__ == '__main__':
+    print(add_item(sys.argv))
