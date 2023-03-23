863370c7f7560f7769f295a247254929a7494ed3
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-23 01:05:20 -0700

    Improve geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defining a class"""
+
+
+class BaseGeometry:
+    """A base class for geometry objects."""
+
+    def area(self):
+        """
+        Calculates the area of the geometry object.
+
+        Raises:
+        NotImplementedError: If the method is not implemented in a subclass.
+        """
+        raise NotImplementedError("area() is not implemented")
