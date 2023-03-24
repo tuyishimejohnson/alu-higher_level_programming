db4e4e1ed584f9204257c3f76012b36e7cece491
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-24 03:08:08 -0700

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,41 +1,39 @@
-#!/usr/bin/python3
-''' prasing the lofg '''
-
+#!/usr/bin/env python3
 
 import sys
 
+# Initialize variables
+file_size = 0
+status_codes = {
+    200: 0,
+    301: 0,
+    400: 0,
+    401: 0,
+    403: 0,
+    404: 0,
+    405: 0,
+    500: 0,
+}
+line_count = 0
 
-def print_status():
-    '''
-        Printing the status of the request
-    '''
-    counter = 0
-    size = 0
-    file_size = 0
-    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
-                    "403": 0, "404": 0, "405": 0, "500": 0}
+# Define a function to print metrics
+def print_metrics():
+    print(f"File size: {file_size}")
+    for status_code in sorted(status_codes.keys()):
+        if status_codes[status_code] > 0:
+            print(f"{status_code}: {status_codes[status_code]}")
+    print()
 
-    for l in sys.stdin:
-        line = l.split()
-        try:
-            size += int(line[-1])
-            code = line[-2]
-            status_codes[code] += 1
-        except:
-            continue
-        if counter == 9:
-            print("File size: {}".format(size))
-            for key, val in sorted(status_codes.items()):
-                if (val != 0):
-                    print("{}: {}".format(key, val))
-            counter = 0
-        counter += 1
-    if counter < 9:
-        print("File size: {}".format(size))
-        for key, val in sorted(status_codes.items()):
-            if (val != 0):
-                print("{}: {}".format(key, val))
+# Read stdin line by line and compute metrics
+try:
+    for line in sys.stdin:
+        line_count += 1
+        ip, _, _, status_code, size = line.split()
+        file_size += int(size)
+        status_codes[int(status_code)] += 1
+        if line_count % 10 == 0:
+            print_metrics()
 
+except KeyboardInterrupt:
+    print_metrics()
 
-if __name__ == "__main__":
-    print_status()
