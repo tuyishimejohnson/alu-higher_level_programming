33f87beb6f8f5e26b7e4992bdd13c4dcd7128483
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-24 03:26:02 -0700

    Log parsing

--- python-input_output/101-stats.py
+++ python-input_output/101-stats.py
@@ -1,46 +1,41 @@
 #!/usr/bin/python3
-"""Importing sys"""
+''' prasing the lofg '''
+
 
 import sys
 
-"""Initialize variables to hold metrics"""
-total_size = 0
-status_counts = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-line_count = 0
 
-try:
-    """Loop through standard input"""
-    for line in sys.stdin:
-        # Increment line count
-        line_count += 1
-        
-        # Split input line by spaces
-        parts = line.split()
+def print_status():
+    '''
+        Printing the status of the request
+    '''
+    counter = 0
+    size = 0
+    file_size = 0
+    status_codes = {"200": 0, "301": 0, "400": 0, "401": 0,
+                    "403": 0, "404": 0, "405": 0, "500": 0}
 
-        # Extract file size and status code
-        size = int(parts[-1])
-        status = int(parts[-2])
+    for l in sys.stdin:
+        line = l.split()
+        try:
+            size += int(line[-1])
+            code = line[-2]
+            status_codes[code] += 1
+        except:
+            continue
+        if counter == 9:
+            print("File size: {}".format(size))
+            for key, val in sorted(status_codes.items()):
+                if (val != 0):
+                    print("{}: {}".format(key, val))
+            counter = 0
+        counter += 1
+    if counter < 9:
+        print("File size: {}".format(size))
+        for key, val in sorted(status_codes.items()):
+            if (val != 0):
+                print("{}: {}".format(key, val))
 
-        # Add file size to total
-        total_size += size
 
-        # Increment status count
-        if status in status_counts:
-            status_counts[status] += 1
-
-        """If 10 lines have been processed, print statistics"""
-        if line_count == 10:
-            print(f"Total file size: {total_size}")
-            for status, count in sorted(status_counts.items()):
-                if count > 0:
-                    print(f"{status}: {count}")
-            print()
-            line_count = 0
-
-except KeyboardInterrupt:
-    # Print final statistics on keyboard interrupt
-    print(f"Total file size: {total_size}")
-    for status, count in sorted(status_counts.items()):
-        if count > 0:
-            print(f"{status}: {count}")
-
+if __name__ == "__main__":
+    print_status()
