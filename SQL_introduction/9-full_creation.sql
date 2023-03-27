d2ccb58d6b0dbba703d197a44110f998760fe1ac
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-27 03:35:27 -0700

    Creating the second table

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,11 @@
 -- Script that creates the second table
-CREATE TABLE IF NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
+CREATE TABLE IF NOT EXISTS second_table (
+    id INT PRIMARY KEY,
+    name VARCHAR(256),
+    score INT
+);
 
-INSERT INTO second_table VALUES (id = 1, name = 'John', score = 10);
-INSERT INTO second_table VALUES (id = 2, name = 'Alex', score = 3);
-INSERT INTO second_table VALUES (id = 3, name = 'Bob', score = 14);
-INSERT INTO second_table VALUES (id = 4, name = 'George', score = 8);
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
