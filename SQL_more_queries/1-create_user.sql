63b46f44661481d444edfb11f626d7251a60eaa3
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-03-28 06:48:30 -0700

    Root user

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,7 @@
 -- Script that creates the Mysql user_0d_1
-CREATE user_0d_1 IF NOT EXISTS;
+CREATE USER
+    IF NOT EXISTS 'user_0d_1'@'localhost'
+    IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES
+   ON *.*
+   TO 'user_0d_1'@'localhost';
