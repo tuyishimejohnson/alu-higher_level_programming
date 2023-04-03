a49efac5d7f6cba59df49c8bab438d7ffeb6e3f8
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 12:24:37 -0700

    What is my status

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Fetches the URL: https://intranet.hbtn.io/status
-with `requests` module
-"""
+"""Script that fetches https://intranet.hbtn.io/status"""
+
 
 import requests
 
+if __name__ == '__main__':
+    url = "https://intranet.hbtn.io/status"
+    request = requests.get(url)
+    text = request.text
+    print("Body response:")
+    print("\t- type: {}".format(type(text)))
+    print("\t- content: {}".format(text))
 
-if __name__ == "__main__":
-    req = requests.get('https://intranet.hbtn.io/status')
-
-    print('Body response:')
-    print('\t- type: {_type}'.format(_type=type(req.text)))
-    print('\t- content: {_content}'.format(_content=req.text))
