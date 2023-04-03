da225508af868559e1f54de0020f45dc3cc3cb42
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 04:29:39 -0700

    CURL headers

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Script that takes a URL as an argument, send get requet and displays the body
-curl -s "$1" -X GET -H "X-HolbertonSchool-User-Id: 98"
+curl -sH "X-School-User-Id: 98" "$1"
