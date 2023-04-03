72fcbb2f642277dc45d00b071bc6eff15ac00ace
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 11:54:58 -0700

    Response header value

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+
+'''Get the URL from the command line arguments'''
+url = sys.argv[1]
+
+'''Send a request to the URL'''
+req = urllib.request.Request(url)
+
+'''Open the URL and read the response headers'''
+with urllib.request.urlopen(req) as response:
+    headers = response.info()
+
+ '''Extract the value of the X-Request-Id variable from the headers'''
+x_request_id = headers.get('X-Request-Id')
+
+# Display the value of the X-Request-Id variable
+print(x_request_id)
+
