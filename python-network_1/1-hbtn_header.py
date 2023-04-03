cfb0db8827199114f55a977b1f9d02e4dff6dc73
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 12:02:13 -0700

    Response header value

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,23 +1,12 @@
 #!/usr/bin/python3
-"""Importing a URL"""
-
+"""Import urllib"""
 
 import urllib.request
 import sys
 
-'''Get the URL from the command line arguments'''
-url = sys.argv[1]
 
-'''Send a request to the URL'''
-req = urllib.request.Request(url)
-
-'''Open the URL and read the response headers'''
-with urllib.request.urlopen(req) as response:
-    headers = response.info()
-
- '''Extract the value of the X-Request-Id variable from the headers'''
-x_request_id = headers.get('X-Request-Id')
-
-# Display the value of the X-Request-Id variable
-print(x_request_id)
-
+"""Display the value of X-Request_Id in the header"""
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        html = response.info()
+        print(html.get("X-Request-Id"))
