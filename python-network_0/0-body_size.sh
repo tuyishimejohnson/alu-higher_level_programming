46eec7c47a4c8bf3604d5bb8380b90ae268d75d7
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 02:58:31 -0700

    CURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # A script that takes in URL sends a request and displays the size of the body.
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+curl -sI "$1" | grep -i Content-Length | cut -d " " -f2
