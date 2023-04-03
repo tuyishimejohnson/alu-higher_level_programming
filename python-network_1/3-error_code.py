2289605eca16ae94696acffa854ac3b727e5becb
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 12:11:01 -0700

    Error code

---
+++ python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Importing urllib and request"""
+
+
+import urllib.request
+import urllib.error
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as e:
+        print("Error code: {}".format(e.code))
