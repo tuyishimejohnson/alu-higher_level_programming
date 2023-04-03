e440e692bf8ce38c873a875bcfc1d2a5857345e2
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 12:46:14 -0700

    Response header value

---
+++ python-network_1/5-hbtn_header.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Importing requests"""
+
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    print(response.headers.get("X-Request-Id"))
