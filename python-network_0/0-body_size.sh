2fce959b9842eb85308346a748bce8cf8c7331b7
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-03 03:16:22 -0700

    CURL body size

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 # This program takes in a URL, sends a request to that URL, and displays the size of the body of the response.
-curl -sI "$1" | grep "Content-Length:" | cut -d' ' -f2
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 <url>"
+  exit 1
+fi
+
+response=$(curl -s -w "%{size_download}" -o /dev/null "$1")
+
+echo "Size of the body of the response: $response bytes"
+
