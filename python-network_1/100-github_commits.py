ea846b1e15b56f2f3acd4b5642952b5131d6f1bd
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-04 04:51:43 -0700

    TIme to interview

--- python-network_1/100-github_commits.py
+++ python-network_1/100-github_commits.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
-"""Python script that takes 2 arguments in order to solve the given challenge"""
+"""List 10 commits (from the most recent to oldest)
+of a repository by user.
+This script use the Github API and print all commits
+by: `<sha>: <author name>` (one by line).
+"""
 
-
-import requests
 from sys import argv
+import requests
 
-if __name__ == '__main__':
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    reponse = requests.get(url)
-    commits = reponse.json()
-    for commit in commits[:10]:
-        print(commit.get('sha'), end=': ')
-        print(commit.get('commit').get('author').get('name'))
+
+if __name__ == "__main__":
+    url = 'https://api.github.com'
+    uri = '{0}/repos/{1}/{2}/commits'.format(url, argv[2], argv[1])
+    req = requests.get(uri).json()
+
+    for com in req[0:10]:
+        print(com['sha'] + ':', com['commit']['author']['name'])
+
