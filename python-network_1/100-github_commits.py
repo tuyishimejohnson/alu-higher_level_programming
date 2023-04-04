de546cb880b8d46255a841177919d1dec4e87b8f
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-04 04:44:06 -0700

    TIme to interview

---
+++ python-network_1/100-github_commits.py
@@ -0,0 +1,13 @@
+"""Python script that takes 2 arguments in order to solve the given challenge"""
+
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
+    reponse = requests.get(url)
+    commits = reponse.json()
+    for commit in commits[:10]:
+        print(commit.get('sha'), end=': ')
+        print(commit.get('commit').get('author').get('name'))
