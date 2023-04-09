b85cba248bca963b72e2b9411c612b212541d838
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-09 03:27:44 -0700

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,40 +1,21 @@
 #!/usr/bin/python3
-"""A module to add two numbers
-This module performs the addition operation between two numbers,
-these numbers can be integers or floats.
-"""
-
 
 def add_integer(a, b=98):
-    """Adds two numbers
-    Performs the addition between two numbers.
-    Args:
-        a (:obj:`int, float`): The first number.
-        b (:obj:`int, float`, optional): The second number.
-    Returns:
-        int: The result of the addition.
     """
-    if type(a) not in (int, float):
-        raise TypeError('a must be an integer')
+    Returns the addition of two integers a and b.
 
-    if type(b) not in (int, float):
-        raise TypeError('b must be an integer')
-
-    a = convert_to_int(a)
-    b = convert_to_int(b)
-    return a + b
-
-
-def convert_to_int(num):
-    """Cast the data type of num parameter
-    Convert a float number to a integer number
     Args:
-        num (:obj:`int, float`): The number to cast.
-    Returns:
-        int: The number casted to integer.
-    """
-    if type(num) is float:
-        num = int(num)
-        return num
+        a: An integer or float number.
+        b: An integer or float number. Defaults to 98.
 
-    return num
+    Returns:
+        An integer: the addition of a and b
+
+    Raises:
+        TypeError: if a or b is not an integer or float.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
