6501e78fd1334ce942972c482a9a4f828d5be1d5
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-09 03:11:22 -0700

    Integers addition

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+def add_integer(a, b=98):
+    """ Adding the two integers """
+    if not a and b int or float:
+        raise TypeError("a must be an integer or b must be an integer")
