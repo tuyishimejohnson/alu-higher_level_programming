a14e74f889025ac3fc57e352251e6f2a518358a8
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-14 05:20:44 -0700

    List relationship

--- python-object_relational_mapping/101-relationship_states_cities_list.py
+++ python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -1,37 +1,26 @@
-#!/usr/bin/python3
-"""
-Script that lists all State objects, and corresponding City objects,
-contained in the database hbtn_0e_101_usa sorted in ascending order
-"""
+#!/usr/bin/env python3
+"""Lists all State objects, and corresponding City objects, contained in the database hbtn_0e_101_usa"""
 
 import sys
-"""Importing sys """
-
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from relationship_state import Base, State
+from relationship_state import State
 from relationship_city import City
 
-if __name__ == "__main__":
-    # Connection setup to the database
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    db_name = sys.argv[3]
-    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".
-                           format(mysql_username, mysql_password, db_name),
-                           pool_pre_ping=True)
+if __name__ == '__main__':
+    if len(sys.argv) != 4:
+        print('Usage: {} <username> <password> <database>'.format(sys.argv[0]))
+        sys.exit(1)
 
-    # Create a configured "Session" class
+    username, password, database = sys.argv[1:]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database),
+                           pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
-    # Create a Session instance
     session = Session()
 
-    # Retrieve all the states sorted by id
     states = session.query(State).order_by(State.id).all()
-
-    # Print each state and their corresponding cities sorted by id
     for state in states:
-        print("{}: {}".format(state.id, state.name))
+        print('{}: {}'.format(state.id, state.name))
         for city in state.cities:
-            print("\t{}: {}".format(city.id, city.name))
+            print('\t{}: {}'.format(city.id, city.name))
+
