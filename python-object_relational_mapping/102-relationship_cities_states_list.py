3229ede2400f29eb0f5e0e305626cdd37e244c99
tuyishime johnson <j.tuyishime4@alustudent.com>
2023-04-14 05:24:28 -0700

    From city

---
+++ python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+Lists all City objects from the database hbtn_0e_101_usa
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == '__main__':
+    mysql_user = sys.argv[1]
+    mysql_pwd = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(mysql_user, mysql_pwd, db_name),
+                           pool_pre_ping=True)
+
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for city, state in session.query(City, State).filter(City.state_id == State.id).order_by(City.id):
+        print('{}: {} -> {}'.format(city.id, city.name, state.name))
