5666d3a3bb32d5aa9695d2a203e56655c00da3fb
root <j.tuyishime4@alustudent.com>
2023-06-07 06:57:56 -0700

    Loop to languages

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-
 const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-
-for (let i = 0; i < myArray.length; i++) {
-
-	console.log(myArray[i]);
-}
+for (let i = 0; i < myArray.length; i++) { Console.log(myArray[i]); }
