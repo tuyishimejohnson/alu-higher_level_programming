4a9b597750c9ac977e957dbdf703d42f57de95dc
root <j.tuyishime4@alustudent.com>
2023-06-07 05:59:40 -0700

    An integer

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -2,6 +2,6 @@
 
 const num = parseInt(process.argv[2]);
 
-if (Number.isNaN(num)) { console.log('Not a number');} 
+if (Number.isNaN(num)) { console.log('Not a number'); } 
 
-else { console.log('My number: ' + num);}
+else { console.log('My number: ' + num); }
