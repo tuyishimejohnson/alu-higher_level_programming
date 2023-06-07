c16955e75244cf57142d7f85b629acc416cb817b
root <j.tuyishime4@alustudent.com>
2023-06-07 07:13:28 -0700

    2 Arguments

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,14 +1,4 @@
 #!/usr/bin/node
-
-
-
-if (process.argv.length<=2) {
-console.log("No argument");
+if (process.argv.length<=2) { console.log("No argument");
 }
-else if (process.argv.length===2) {
-console.log("Argument found");
-}
-
-else {
-console.log("Arguments found");
-}
+else if (process.argv.length===2) { console.log("Argument found"); } else { console.log("Arguments found"); }
