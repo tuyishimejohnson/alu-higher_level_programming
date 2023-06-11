9937116aa79a2692b3f485e5d35be1577b6d48cb
root <j.tuyishime4@alustudent.com>
2023-06-11 03:24:36 -0700

    Number conversion

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,3 @@
 #!/usr/bin/node
-exports.converter = function(base) {
-  return function(number) { return number.toString(base); };
+exports.converter = function(base) { return function(number) { return number.toString(base); };
 };
