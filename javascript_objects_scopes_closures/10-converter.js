d44d206c6c29f78d1a124be48d32983647106c67
root <j.tuyishime4@alustudent.com>
2023-06-11 03:06:14 -0700

    Number conversion

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,18 +1,6 @@
 #!/usr/bin/node
 exports.converter = function(base) {
-  return function convertToBase(number) {
-    return number === 0 ? '0' : (number < 0 ? '-' : '') + convert(Math.abs(number));
-
-    function convert(number) {
-      const digits = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-      const baseDigits = digits.slice(0, base);
-
-      let result = '';
-      while (number > 0) {
-        result = baseDigits[number % base] + result;
-        number = Math.floor(number / base);
-      }
-      return result;
-    }
+  return function(number) {
+    return number.toString(base);
   };
 };
