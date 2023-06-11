ead8fad05ae180ee163c753d283fc652db9691ed
root <j.tuyishime4@alustudent.com>
2023-06-11 03:27:00 -0700

    Number conversion

--- javascript_objects_scopes_closures/10-converter.js
+++ javascript_objects_scopes_closures/10-converter.js
@@ -1,3 +1,7 @@
 #!/usr/bin/node
-exports.converter = function(base) { return function(number) { return number.toString(base); };
+exports.converter = function(base) {
+  return function(number) {
+    return number.toString(base);
+  };
 };
+
