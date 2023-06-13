2a330240093a8afcfef697da53bd28b2839d9e58
tuyishimejohnson <j.tuyishime4@alustudent.com>
2023-06-13 07:36:24 +0000

    Write me

--- javascript-web_scraping/1-writeme.js
+++ javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const myFile = process.argv[2];
-const content = process.argv[3];
 const fs = require('fs');
-fs.writeFile(myFile, content, err => {
-  if (err) {
-    console.log(err);
+const file = process.argv[2];
+const write = process.argv[3];
+fs.writeFile(file, write, 'utf8', function (error) {
+  if (error) {
+    console.log(error);
   }
 });
