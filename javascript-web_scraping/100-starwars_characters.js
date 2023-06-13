2a6bac6b82bcb8da27c7cef2192006748d3cf04b
tuyishimejohnson <j.tuyishime4@alustudent.com>
2023-06-13 11:27:09 +0000

    Starwars characters

--- javascript-web_scraping/100-starwars_characters.js
+++ javascript-web_scraping/100-starwars_characters.js
@@ -4,12 +4,14 @@ const request = require('request');
 // Function to fetch characters of a Star Wars movie
 function fetchCharacters (movieId) {
   const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
-    request(apiUrl, (error, response, body) => {
+  
+  request(apiUrl, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const movieData = JSON.parse(body);
       const characters = movieData.characters;
-     // Fetch character names
-      characters.forEach(characterUrl =>
+      
+      // Fetch character names
+      characters.forEach(characterUrl => {
         request(characterUrl, (error, response, body) => {
           if (!error && response.statusCode === 200) {
             const characterData = JSON.parse(body);
@@ -24,6 +26,7 @@ function fetchCharacters (movieId) {
     }
   });
 }
+
 // Fetch characters of the specified movie
 const movieId = process.argv[2];
 fetchCharacters(movieId);
